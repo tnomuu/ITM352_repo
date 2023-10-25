d39c32ce51319d5c306e169b6a7c23bdd59604a7
tnomuu <tnom808@hawaii.edu>
2023-10-24 15:18:56 -1000

    commit to github

--- Lab 9/lab9ex6.js
+++ Lab 9/lab9ex6.js
@@ -34,12 +34,12 @@ function checkIt(item, index) {
 // callback function
 pieces.forEach(checkIt); */
 
-function download(url) {
+function download(url, callback) {
     setTimeout(() => {
         // script to download the picture here
         console.log(`Downloading ${url} ...`);
         picture_data = "image data:XOXOXO";
-        return picture_data;
+        callback(picture_data);
     }, 3* 1000);
     
 }
@@ -49,4 +49,4 @@ function process(picture) {
 }
 
 let url = 'https://www.example.com/big_pic.jpg';
-process( download(url) );
+download(url, process);
