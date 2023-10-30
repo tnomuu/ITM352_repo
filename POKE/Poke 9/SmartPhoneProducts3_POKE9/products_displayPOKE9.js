6ab8c30c94872982ba5890104bf6c22a8d4e22c2
tnomuu <tnom808@hawaii.edu>
2023-10-29 19:23:15 -1000

    commit to github

---
+++ POKE/Poke 9/SmartPhoneProducts3_POKE9/products_displayPOKE9.js
@@ -0,0 +1,199 @@
+//define store name and push to DOM
+const store_name="Trent Nomura";
+let first_name="Trent"
+let last_name="Nomura"
+
+//name to h1 tags
+top_title1.innerHTML=('<b>' + first_name + '<b>' + '<i>' + last_name + '<i>')
+
+//top title to h2 tags 
+top_title2.innerHTML=(store_name + "'s Used Smart Phone Store");
+ 
+//bottom title to footer tags
+bottom_title.innerHTML=("Your One Stop Shop For Used Phones - "+store_name+"'s");
+
+//initialize hits and spins and send to the DOM
+let hits=0;
+let spins=1;
+//let wins;
+let over_half=false
+hits_span.innerHTML = hits;
+spins_span.innerHTML = spins;
+
+// product information objects POKE9
+
+     let product1 = {
+        brand: "HTC",
+        price: 40.00,
+        image: "http://dport96.github.io/ITM352/morea/080.flow-control-II/HTC.jpg"
+    }
+
+     let product2 = {
+        brand: "Apple",
+        price: 75.00,
+        image: "http://dport96.github.io/ITM352/morea/080.flow-control-II/iphone-3gs.jpg"
+    }
+
+     let product3 = {
+        brand: "Nokia",
+        price: 35.00,
+        image: "http://dport96.github.io/ITM352/morea/080.flow-control-II/Nokia.jpg"
+    }
+
+     let product4 = {
+        brand: "Samsung",
+        price: 45.00,
+        image: "http://dport96.github.io/ITM352/morea/080.flow-control-II/Samsung.jpg"
+    }
+
+     let product5 = {
+        brand: "Blackberry",
+        price: 10.00,
+        image: "http://dport96.github.io/ITM352/morea/080.flow-control-II/Blackberry.jpg"
+    }
+
+    let products = [product1, product2, product3, product4, product5]
+
+    for (i = 0; i < products.length; i++) {
+        document.querySelector('.main').innerHTML += `
+        <section class="item" onmouseover="changeClassName(this);" 
+        onclick="resetClassName(this);">
+            <h2>${products[i].brand}</h2>
+            <p2>${products[i].price}</p2>
+            <img src="${products[i].image}"/>
+        </section>`;
+    }
+
+//two functions that change item name to rotate (onmouseover) and to stop (onmouseclick), updates hits and spins count also calculating the hits/spins ratio, sending both to DOM
+function changeClassName(element) {
+    if(element.className=='item'){
+        spins=spins+1;
+        element.className = 'item rotate';
+    }
+
+    if(spins<2*hits&&hits<spins){
+        //wins=true;
+        over_half=true
+    } else {
+        //wins=false
+    }
+    //win_span.innerHTML=wins;
+    win_span.innerHTML=over_half;
+    spins_span.innerHTML=spins;
+    hit_spin_span.innerHTML=(hits/spins).toFixed(2);
+    // -- Winning progress depends on hits/spins
+    let hits_spins_ratio = hits/spins;
+    let progress;
+    /*if ( hits_spins_ratio > 0 ) {
+        progress = 'On your way!';
+        if ( hits_spins_ratio >= 0.25 ) {
+            progress = 'Almost there!';
+            if ( hits_spins_ratio >= 0.5 ) {
+                if( hits < spins) { 
+                    progress = 'You win!';
+                }
+            }   
+        }
+    }
+    else {
+        progress = 'Get going!' ;
+    }
+    // code from class
+    if (hits_spins_ratio >= 0.5 && hits<spins) {
+        progress='You win!';
+    } else if (hits_spins_ratio >= 0.25) {
+        progress='Almost there!';
+    } else if (hits_spins_ratio > 0) {
+        progress='On your way!';
+    } else {
+        progress='Get going!';
+    }*/
+
+    // code from Bing
+    if ( hits_spins_ratio >= 0.5 && hits < spins) {
+        progress = 'You win!';
+    } else if ( hits_spins_ratio >= 0.25 ) {
+        progress = 'Almost there!';
+    } else if ( hits_spins_ratio > 0) { 
+        progress = 'On your way!';
+    } else {
+        progress = 'Get going!' ;
+    }
+    win_span.innerHTML=progress;
+}
+    function resetClassName(element) {
+    if(element.className=='item rotate'){
+        hits=hits+1;
+        element.className = 'item';
+    } else {
+        changeClassName(element);
+    }
+
+    if(spins<2*hits&&hits<spins){
+        //wins=true;
+        over_half=true
+    } else {
+        //wins=false
+    }
+    //win_span.innerHTML=wins;
+    win_span.innerHTML=over_half;
+    hits_span.innerHTML=hits;
+    hit_spin_span.innerHTML=(hits/spins).toFixed(2);
+    // -- Winning progress depends on hits/spins
+    let hits_spins_ratio = hits/spins;
+    let progress;
+    /*if ( hits_spins_ratio > 0 ) {
+        progress = 'On your way!';
+        if ( hits_spins_ratio >= 0.25 ) {
+            progress = 'Almost there!';
+            if ( hits_spins_ratio >= 0.5 ) {
+                if( hits < spins) { 
+                    progress = 'You win!';
+                }
+            }   
+        }
+    }
+    else {
+        progress = 'Get going!' ;
+    }
+    // code from class
+    if (hits_spins_ratio >= 0.5 && hits<spins) {
+        progress='You win!';
+    } else if (hits_spins_ratio >= 0.25) {
+        progress='Almost there!';
+    } else if (hits_spins_ratio > 0) {
+        progress='On your way!';
+    } else {
+        progress='Get going!';
+    }*/
+
+    // code from Bing
+    if ( hits_spins_ratio >= 0.5 && hits < spins) {
+        progress = 'You win!';
+    } else if ( hits_spins_ratio >= 0.25 ) {
+        progress = 'Almost there!';
+    } else if ( hits_spins_ratio > 0) { 
+        progress = 'On your way!';
+    } else {
+        progress = 'Get going!' ;
+    }
+    win_span.innerHTML=progress;
+} 
+
+
+//table that displays name of store, my name, current date and time
+        let line=1;
+        now = new Date (); 
+        year = now.getFullYear();
+        hours = now.getHours();
+        minutes = now.getMinutes();
+        ampm = (hours < 12)?'AM':'PM';
+        hours = (hours + 11) % 12+1;
+    document.write(`
+        <table border="1" style="margin-left: auto; margin-right: auto; text-align: center;">
+        <tr><th colspan="2"><h1>Your One Stop For Used Phones - ${first_name[0]}.${last_name[0]}'s</h1></th></tr>
+        <tr><td>1.</td><td><br />Copyright @ ${first_name + '' + last_name}</td></tr>
+        <tr><td>2.</td><td>${now.getFullYear()}</td></tr>
+        <tr><td>3.</td><td>${hours}:${minutes}${ampm}</td></tr>
+        </table>
+    `);
